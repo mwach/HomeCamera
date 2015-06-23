7262af05018b35c7c73ca71b314e756eea685f6f
Marcin Wachowiak <marcin.wachowiak@mobica.com>
2015-06-23 22:43:35 +0200

    settings development

--- app/src/main/java/com/mawa/homecamera/SettingsActivity.java
+++ app/src/main/java/com/mawa/homecamera/SettingsActivity.java
@@ -2,22 +2,30 @@ package com.mawa.homecamera;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.mawa.homecamera.accordion.utils.FontUtils;
+import com.mawa.homecamera.accordion.widget.AccordionView;
 
 
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.settings_menu, menu);
-        return true;
+        final AccordionView v = (AccordionView) findViewById(R.id.accordion_view);
+
+//        LinearLayout ll = (LinearLayout) v.findViewById(R.id.settings_layout);
+//        TextView tv = new TextView(this);
+//        tv.setText("Added in runtime...");
+//        FontUtils.setCustomFont(tv, getAssets());
+//        ll.addView(tv);
+
     }
 }
