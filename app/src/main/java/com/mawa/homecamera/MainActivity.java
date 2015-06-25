17e9d15e4105c21ed17bc30f3acca0a927e6cdd1
Marcin Wachowiak <marcin.wachowiak@mobica.com>
2015-06-25 09:57:55 +0200

    camera support added

--- app/src/main/java/com/mawa/homecamera/MainActivity.java
+++ app/src/main/java/com/mawa/homecamera/MainActivity.java
@@ -1,19 +1,84 @@
 package com.mawa.homecamera;
 
 import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.mawa.homecamera.camera.Preview;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Preview preview;
+    Camera camera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton recordButton = (ImageButton) findViewById(R.id.record_button);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecording();
+            }
+        });
+
+        preview = new Preview(this, (SurfaceView) findViewById(R.id.surface_view));
+        preview.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        ((RelativeLayout) findViewById(R.id.camera_layout)).addView(preview);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int numCams = Camera.getNumberOfCameras();
+        if (numCams > 0) {
+            try {
+                camera = Camera.open(0);
+                camera.startPreview();
+                preview.setCamera(camera);
+            } catch (RuntimeException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (camera != null) {
+            camera.stopPreview();
+            preview.setCamera(null);
+            camera.release();
+            camera = null;
+        }
+        super.onPause();
+    }
+
+    private void resetCam() {
+        camera.startPreview();
+        preview.setCamera(camera);
+    }
+
+    private void startRecording() {
+
+        Toast.makeText(MainActivity.this, getString(R.string.recording_started), LENGTH_LONG).show();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/mawa/homecamera/processing/video/Detector.java
@@ -0,0 +1,7 @@
+package com.mawa.homecamera.processing.video;
+
+/**
+ * Created by mawa on 24/06/15.
+ */
+public class Detector {
+}
